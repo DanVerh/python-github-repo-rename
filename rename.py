625698dafc964f8a7604b0a3c62a3b370e7e0088
DanVerh <danverh29@gmail.com>
2023-04-07 16:44:18 +0300

    match case edited

--- rename.py
+++ rename.py
@@ -1,9 +1,10 @@
 import requests
+import re
 
 # Replace these values with your own information
-token = 'YOUR_ACCESS_TOKEN'
-repo_filter = 'substring'
-new_prefix = 'new-prefix'
+token = 'TOKEN' # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+repo_filter = "FILTER_FOR_REPOS"
+new_prefix = "NEW_PREFIX"
 
 headers = {'Authorization': f'token {token}'}
 
@@ -11,11 +12,15 @@ headers = {'Authorization': f'token {token}'}
 url = 'https://api.github.com/user/repos'
 response = requests.get(url, headers=headers)
 repos = response.json()
+print("All repos:")
+print(repos)
 
-# Rename each repository that starts with the old prefix
+# Rename each repository that contains metioned substring in repo_filter
 for repo in repos:
-    if repo['name'].find(repo_filter):
+    if re.search(repo_filter, repo['name']):
         old_name = repo['name']
+        match = re.search(repo_filter, repo['name'])
+        print(repo['name'])
         new_name = new_prefix + repo['name']
         url = f'https://api.github.com/repos/{repo["full_name"]}'
         data = {'name': new_name}
